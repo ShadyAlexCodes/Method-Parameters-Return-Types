cd9d1c6be1935fa9c7032addfdec3759e5732642
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-10-18 11:12:40 -0600

    Started a simple calculator

--- app/src/main/java/com/example/methodparameters_returntypes/MainActivity.java
+++ app/src/main/java/com/example/methodparameters_returntypes/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.methodparameters_returntypes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onInteract(View view) {
+        EditText etNumberOne = (EditText) findViewById(R.id.numberOne);
+        int firstNumber = Integer.parseInt(etNumberOne.getText().toString());
+
+        EditText etNumberTwo = (EditText) findViewById(R.id.numberTwo);
+        int secondNumber = Integer.parseInt(etNumberTwo.getText().toString());
+
+        TextView finalValue = (TextView) findViewById(R.id.finalValue);
+        finalValue.setText(Integer.toString(firstNumber + secondNumber));
     }
 }
